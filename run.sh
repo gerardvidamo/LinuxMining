ca57a3f69adc1d1765137cf6f1f8142a6e47bb45
Gerard Vidamo <gerard@stacksdiscovery.com>
2018-10-29 15:54:08 -0600

    Updated run.sh file

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ STATRUM_PORT="6789"
 WALLET_ADDRESS="RM6SG2HK5T33y2eU1msPioSpUzd8itcNrS"
 MINERS_NAME="miner-"
 
-./ccminer -a x16r -o stratum+tcp://${MINING_SERVER}:${STATRUM_PORT} -u ${WALLET_ADDRESS} -p ${MINERS_NAME}
+ccminer/ccminer -a x16r -o stratum+tcp://${MINING_SERVER}:${STATRUM_PORT} -u ${WALLET_ADDRESS} -p ${MINERS_NAME}
