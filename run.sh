4c6d93f7374a1d60c83599867d3b326c09bcbd7b
gerardvidamo <cpegerardvidamo@gmail.com>
2018-10-30 18:03:04 -0600

    Initial commit

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 MINING_SERVER="us.ravenminer.com"
 STATRUM_PORT="6789"
 WALLET_ADDRESS="RM6SG2HK5T33y2eU1msPioSpUzd8itcNrS"
-MINERS_NAME="miner-"
+MINERS_NAME="miner-1"
 
-ccminer/ccminer -a x16r -o stratum+tcp://${MINING_SERVER}:${STATRUM_PORT} -u ${WALLET_ADDRESS} -p ${MINERS_NAME}
+ccminer-linux/ccminer -a x16r -o stratum+tcp://${MINING_SERVER}:${STATRUM_PORT} -u ${WALLET_ADDRESS} -p ${MINERS_NAME}
